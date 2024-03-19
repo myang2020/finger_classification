22531e2ed3780b6bee47c7d19e4dca2d9dc7b5ec
Max Yang <>
2024-03-19 17:15:22 +0800

    Split train/test

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from setup_data import Data
 import torch
 import matplotlib.pyplot as plt
 import os
+from torch.utils.data import DataLoader
 
 
 def main():
     data = Data(os.path.abspath("/Volumes/DATAUSB/archive"))
     figure = plt.figure(figsize=(8, 8))
     cols, rows = 3, 3
+
+    train_size = int(0.8 * len(data.labels))
+    test_size = len(data.labels) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(data, [train_size, test_size])
+    print(len(train_dataset))
     for i in range(1, cols * rows + 1):
-        sample_idx = torch.randint(len(data), size=(1,)).item()
-        img, label = data[sample_idx]
+        sample_idx = torch.randint(len(train_dataset), size=(1,)).item()
+        img, label = train_dataset[sample_idx]
         figure.add_subplot(rows, cols, i)
         plt.title(label)
         plt.axis("off")
         plt.imshow(img.squeeze(), cmap="gray")
     plt.show()
+
 
 if __name__ == "__main__":
     main()
